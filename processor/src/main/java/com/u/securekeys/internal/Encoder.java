47c4e725b2fb1d18aaf07099d3210c151d451a11
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-18 22:51:08 -0300

    Use sha256 for hash

--- processor/src/main/java/com/u/securekeys/internal/Encoder.java
+++ processor/src/main/java/com/u/securekeys/internal/Encoder.java
@@ -1,6 +1,10 @@
 package com.u.securekeys.internal;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -11,6 +15,14 @@ import javax.xml.bind.DatatypeConverter;
  * Created by saguilera on 3/3/17.
  */
 public class Encoder {
+
+    private static String hash = "SHA-256";
+
+    //Charset
+    private static String utf8 = "UTF-8";
+
+    //Radix for the hash
+    private static final int STRING_RADIX_REPRESENTATION = 16;
 
     // Initial vector for AES cipher
     private static final byte initialVectorBytes[] = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04,
@@ -26,12 +38,29 @@ public class Encoder {
 
     public Encoder() {}
 
-    public String encode(String value) {
+    /**
+     * hash a string using hash mode.
+     * @param name string to hash
+     * @return string with the hashed name
+     */
+    private static String hash(String name) {
         try {
-            return DatatypeConverter.printBase64Binary(aes(value.getBytes()));
+            MessageDigest m = MessageDigest.getInstance(hash);
+            m.update(name.getBytes(Charset.forName(utf8)));
+            byte[] digest = m.digest();
+            BigInteger bigInt = new BigInteger(1, digest);
+            return bigInt.toString(STRING_RADIX_REPRESENTATION);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String encode(String what) {
+        try {
+            return DatatypeConverter.printBase64Binary(aes(what.getBytes()));
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Couldnt encode value: " + value, e);
+            throw new RuntimeException("Couldnt encode value: " + what, e);
         }
     }
 
