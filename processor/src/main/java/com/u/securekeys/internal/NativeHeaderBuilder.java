07966ca760eb96a3baacb4ca2c30962592bf3cac
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-30 02:11:12 -0300

    Writing the .h file correctly

--- processor/src/main/java/com/u/securekeys/internal/NativeHeaderBuilder.java
+++ processor/src/main/java/com/u/securekeys/internal/NativeHeaderBuilder.java
@@ -1,8 +1,9 @@
-package com.u.securekeys;
+package com.u.securekeys.internal;
 
 import java.io.Writer;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class NativeHeaderBuilder {
 
@@ -12,36 +13,43 @@ public class NativeHeaderBuilder {
     private String fileName;
 
     public NativeHeaderBuilder(String fileName) {
-        imports = new ArrayList<String>();
-        defines = new ArrayList<String>();
+        imports = new ArrayList<>();
+        defines = new ArrayList<>();
 
         this.fileName = fileName;
     }
 
     public void addImport(String libName) {
-        orderedLines.add("#include <" + libName + ">\n")
+        imports.add("#include <" + libName + ">\n");
     }
 
     public void addDefine(String name, String value) {
-        orderedLines.add("#define " + name + " " + value + "\n");
+        defines.add("#define " + name + " " + value + "\n");
     }
 
-    public void writeTo(Writer writer) {
-        writer.append("// Created by Santiago Aguilera\n");
+    public void writeTo(Writer writer) throws IOException {
+        writer.append("// Created by SecureKeys Annotation Processor - Santiago Aguilera\n\n");
         writer.append("#ifndef SECUREKEYS_EXTERN_" + fileName.toUpperCase() + "_H\n");
         writer.append("#define SECUREKEYS_EXTERN_" + fileName.toUpperCase() + "_H\n");
+
+        writer.append("\n");
 
         for (String line : imports) {
             writer.append(line);
         }
 
+        writer.append("\n");
+
         for (String line : defines) {
             writer.append(line);
         }
 
+        writer.append("\n");
+
         writer.append("#endif //SECUREKEYS_EXTERN_" + fileName.toUpperCase() + "_H\n");
 
         writer.flush();
+        writer.close();
     }
 
 }
